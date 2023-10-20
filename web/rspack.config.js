73946b1476da92240e3584114d2b5af78b61d678
Jerry Jiang <jerry.jiang@discordapp.com>
2023-10-20 01:26:36 -0700

    Install prettier

--- web/rspack.config.js
+++ web/rspack.config.js
@@ -2,23 +2,23 @@
  * @type {import('@rspack/cli').Configuration}
  */
 module.exports = {
-	context: __dirname,
-	entry: {
-		main: "./src/main.tsx"
-	},
-	builtins: {
-		html: [
-			{
-				template: "./index.html"
-			}
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				type: "asset"
-			}
-		]
-	}
+  context: __dirname,
+  entry: {
+    main: "./src/main.tsx",
+  },
+  builtins: {
+    html: [
+      {
+        template: "./index.html",
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+    ],
+  },
 };
