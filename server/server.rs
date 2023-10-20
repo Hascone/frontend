898211db267d6f9d7122cb0ac3981ada9b288de3
Amrit Kooner <amrit_k729@hotmail.com>
2023-10-20 02:07:06 -0700

    Add socket.

--- server/server.rs
+++ server/server.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello World!");
+    let listener = TcpListener::bind("localhost:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
